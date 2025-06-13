441e9b48dc51ca2661761f93acc129c4305ca112
augusto5959 <augustoviteri98@gmail.com>
2025-06-12 20:57:18 -0500

    correcciones

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,6 +12,7 @@ android {
     namespace = "com.example.prueba2_mov3"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -31,6 +32,7 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        minSdk = 23
     }
 
     buildTypes {
--- lib/screens/WelcomeScreen.dart
+++ lib/screens/WelcomeScreen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:prueba2_mov3/auth/LoginScreen.dart';
+import 'package:prueba2_mov3/auth/RegisterScreen.dart';
 
 class Welcome extends StatelessWidget {
   const Welcome({super.key});
@@ -22,11 +24,11 @@ class Welcome extends StatelessWidget {
                 style: TextStyle(fontSize: 16, color: Colors.grey)),
             const SizedBox(height: 20),
             ElevatedButton(
-              onPressed: ()=>(),
+              onPressed: ()=>Navigator.push(context, MaterialPageRoute(builder: (context) => const Login())),
               child: const Text('LOGIN'),
             ),
             ElevatedButton(
-              onPressed: ()=>(),
+              onPressed: ()=>Navigator.push(context, MaterialPageRoute(builder: (context) => const Register())),
               child: const Text('REGISTRARSE'),
             ),
           ],
